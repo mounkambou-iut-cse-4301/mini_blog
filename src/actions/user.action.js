768a38a3100b2403f60d16e64209d1cd7ae4912c
Abdel Karim Mounkambou <abdelkarim@iut-dhaka.edu>
2021-06-22 12:20:46 +0100

    Initial commit

---
+++ src/actions/user.action.js
@@ -0,0 +1,29 @@
+import axios from "axios"
+
+export const GET_USER = "GET_USER"
+export const ADD_USER_LIKE = "ADD_USER_LIKE"
+
+export const getUsers = () => {
+    return (dispatch) => {
+        return axios
+            .get("http://localhost:3000/users")
+            .then((res) => {
+                dispatch({ type: GET_USER, payload: res.data });
+            })
+            .catch((err) => console.log(err));
+    };
+};
+
+export const addUserLike = (data) => {
+    return (dispatch) => {
+      return axios({
+        method:"put",
+        url:`http://localhost:3000/posts/${data.id}`,
+        data:{...data},
+      })
+        .then((res) => {
+          dispatch({ type: ADD_USER_LIKE, payload: {...data} });
+        })
+        .catch((err) => console.log(err));
+    };
+  };
